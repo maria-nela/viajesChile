5ce1018d3519fde5c1100e7238a88c81ba71deaa
Marianela <marian.lobello@mail.com>
2022-11-19 10:52:52 -0300

    elementos Boostrap js y JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,38 @@ $(function (){
         }
     });
 });
+
+
+
+/*---Para coultar texto de las card ---*/
+$(document).ready(function()
+{
+$(".titulo-tarjeta").on( "click", function() {	 
+    $('.texto-tarjeta').toggle();
+     });
+});
+
+/*---Resaltar texto de las cards---*/
+
+    $(".titulo-tarjeta").click(function(){
+    $(".titulo-tarjeta").css({'text-shadow': '3px 3px 6px rgb(9, 33, 108)','font-weight': '500'});
+    });
+ 
+
+/*---Mensaje de alerta---*/
+    var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Genial, enviaste el mensaje &#128522', 'success')
+  })
+}
+
